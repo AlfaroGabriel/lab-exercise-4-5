678bcaf13cc10aa4887ff4a5cfaaa7e152d9d19c
Gabriel Mari B. Alfaro <Alfaro-Ga@ndmu.local>
2024-04-17 17:16:48 +0800

    init

---
+++ lib/ui/recipe_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import '../network/recipe_model.dart';
+
+
+Widget recipeCard(APIRecipe recipe) {
+  return Card(
+    elevation: 4.0,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(6.0),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(6.0), topRight: Radius.circular(6.0)),
+          child: 
+          //  CachedNetworkImage(
+          //  imageUrl: recipe.image,
+          //  imageBuilder: (context, imageProvider) => Container(
+          //    decoration: BoxDecoration(
+          //      image:DecorationImage(
+          //      image: imageProvider,
+          //      fit: BoxFit.cover
+          //      ),
+          //    ),
+          //  ),
+          //  height: 210, 
+          //  width: 350,
+          //  placeholder: (context, url) => const CircularProgressIndicator(),
+          //  errorWidget: ((context, url, error) => const Icon(Icons.error)),  
+          //    ),
+          Image.network(recipe.image,  height: 200,fit: BoxFit.fill)
+        ),
+        const SizedBox(
+          height: 12.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            recipe.label,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Padding(
+ padding: const EdgeInsets.only(left: 8.0),
+ child: Text(
+ getCalories(recipe.calories),
+ style: const TextStyle(
+ fontWeight: FontWeight.normal,
+ fontSize: 11,
+ ),
+ ),
+),
+        const Padding(
+          padding: EdgeInsets.only(left: 8.0),
+          child: Text(
+            '320CAL',
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 11,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+      ],
+    ),
+  );
+}
